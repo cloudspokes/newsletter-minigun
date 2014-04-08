8e66b3a50af62a87975c4919c5b921ba6266851d
creditforce <jeff@m2bllc.com>
2014-04-08 07:08:08 -0400

    added show

--- controllers/templates.js
+++ controllers/templates.js
@@ -1,3 +1,5 @@
+var _ = require('underscore');
+
 var Template = require('../models/Template');
 
 /**
@@ -7,14 +9,19 @@ var Template = require('../models/Template');
 
 exports.index = function(req, res) {
   Template.find({}, function (err, records) {
-    if (err) {
-      req.flash('errors', { msg: 'Crap! Could not find templates' });
-      return res.redirect('/');
-    }
+    if (err) { req.flash('errors', { msg: 'Crap! Could not find templates.' }); }
     res.render('templates/index', {
       title: 'Email templates', records: records
     });
    });
+};
+
+exports.show = function(req, res) {
+  Template.findOne({'_id': req.params.id}, function(err, item) {
+    res.render('templates/show', {
+      title: 'Template details', record: item
+    });
+  });
 };
 
 exports.new = function(req, res) {
@@ -32,7 +39,7 @@ exports.create = function(req, res) {
   });
   // save the record and redirect
   template.save(function(err) {
-    if (err) req.flash('errors', { msg: 'Crap! Could not save template' });
+    if (err) req.flash('errors', { msg: 'Crap! Could not save the template.' });
     return res.redirect('/templates');
   });
 };
@@ -46,43 +53,9 @@ exports.edit = function(req, res) {
 };
 
 exports.update = function(req, res) {
-  console.log('====== running put')
-  Template.findOne({'_id': req.params.id}, function(err, item) {
-
-    // update the values
-    item.name = req.body.name
-    item.body = req.body.body;
-    item.type = req.body.type;
-    item.availableForUse = req.body.available;
-
-    Template.update({'_id': req.params.id}, item, {safe:true}, function(err, result) {
-      if (err) {
-        req.flash('errors', { msg: 'Crap! Could not save template' });
-        console.log(err);
-      }
-      return res.redirect('/templates');
-    });
-  });
-};
-
-// this just be new
-exports.create1 = function(req, res) {
-
-  var template = new Template({
-    name: 'The Best Template',
-    body: 'Hello, {{name}}'
-  });
-
-  template.save(function(err) {
-    if (err) {
-      console.log(err);
-      req.flash('errors', { msg: 'Crap! Could not save template' });
-      return res.redirect('/');
-    }
-  });
-
-  console.log('======= calling new template');
-  res.render('templates', {
-    title: 'Templates'
+  var template = _.omit(req.body, '_csrf');
+  Template.update({'_id': req.params.id}, template, {raw:false}, function(err, result) {
+    if (err) { req.flash('errors', { msg: 'Crap! Could not save template.' }); }
+    return res.redirect('/templates');
   });
 };
